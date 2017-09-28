54b1b3f566b321fd1fe616f0c920c51f4e2fde23
Patty Case <>
2017-09-27 17:44:13 -0700

    Initial commit of PhotoAlbum app

--- app/src/main/java/com/azurehorsecreations/shopper/presentation/ui/navigation/INavigator.java
+++
@@ -1,9 +0,0 @@
-package com.azurehorsecreations.shopper.presentation.ui.navigation;
-
-/**
- * Created by pattycase on 9/14/17.
- */
-
-public interface INavigator {
-    void launchActivity();
-}
--- app/src/main/java/com/azurehorsecreations/shopper/utils/CallbackReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.azurehorsecreations.shopper.utils;
-
-/*
- * CallbackReceiver interface for callbacks to handle data
- */
-
-public interface CallbackReceiver {
-    public void handleResultData(Object result);
-}
